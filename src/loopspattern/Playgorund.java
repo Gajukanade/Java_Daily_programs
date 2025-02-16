54efa86a4d9044988b7344635e93ca00054486cf
GK <gajendrakanade77@gmail.com>
2025-02-16 19:58:56 +0530

    playgound loop pattern pract

---
+++ src/loopspattern/Playgorund.java
@@ -0,0 +1,16 @@
+package loopspattern;
+
+public class Playgorund {
+
+	public static void main(String[] args) {
+		
+
+		for (int i=1;i<=5;i++) {
+			for(int j=5; j>=i;j--) {
+				System.out.print(" #");
+			}
+			System.out.println();
+		}
+	}
+
+}
